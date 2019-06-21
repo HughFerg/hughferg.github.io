ff2bd26c0a3e74f44e92a80398153d3ee05ee96e
hughferg <hughferguson25@gmail.com>
2019-06-21 15:41:21 -0400

    new song

--- scripts/zine.js
+++ scripts/zine.js
@@ -5,24 +5,38 @@ window.onload=function(){
         changeImg();
     });
 
-    function changeImg() {
+    let img1 = new Image();
+    let img2 = new Image();
+    let img3 = new Image();
+    let img4 = new Image();
+    let img5 = new Image();
+    let img6 = new Image();
 
-        let img1 = document.getElementById('img1');
+    img1.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg';
+    img2.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg';
+    img3.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg';
+    img4.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg';
+    img5.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg';
+    img6.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg';
+}
 
-        let imgList = ['https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg',
-            'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg',
-            'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg',
-            'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg',
-            'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg',
-            'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg'
-        ];
+function changeImg() {
 
-        let current = imgList.indexOf(img1.src);
+    let img1 = document.getElementById('img1');
 
-        if (current == imgList.length - 1) {
-        img1.src = imgList[0];
-        } else {
-            img1.src = imgList[current + 1];
-        }
+    let imgList = ['https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg',
+        'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg',
+        'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg',
+        'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg',
+        'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg',
+        'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg'
+    ];
+
+    let current = imgList.indexOf(img1.src);
+
+    if (current == imgList.length - 1) {
+    img1.src = imgList[0];
+    } else {
+        img1.src = imgList[current + 1];
     }
 }
