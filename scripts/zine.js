cde7163f063b6b62792276fad3e4bc862a3a2694
hughferg <hughferguson25@gmail.com>
2019-07-04 02:15:25 -0400

    new css, or something

--- scripts/zine.js
+++ scripts/zine.js
@@ -1,3 +1,14 @@
+let img1, img2, img3, img4, img5, img6, img7 = document.createElement("img");    
+
+img1.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg';
+img2.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg';
+img3.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg';
+img4.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg';
+img5.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg';
+img6.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg';
+img7.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-7.jpg';
+
+let imgList = [img7,img1,img6,img2,img5,img3,img4];
 
 window.onload=function(){
 
@@ -5,33 +16,25 @@ window.onload=function(){
         changeImg();
     });
 
-    let img1 = new Image();
-    let img2 = new Image();
-    let img3 = new Image();
-    let img4 = new Image();
-    let img5 = new Image();
-    let img6 = new Image();
+    // imgList.forEach(img => img.id = 'img1');
 
-    img1.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg';
-    img2.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg';
-    img3.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg';
-    img4.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg';
-    img5.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg';
-    img6.src = 'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg';
+    document.getElementsByClassName("zine").innerHTML = imgList[0];
+
+    imgList[0].style.visibility = 'visible';
 }
 
 function changeImg() {
 
     let img1 = document.getElementById('img1');
 
-    let imgList = ['https://hugh-ferguson.com/assets/img/zine/test-page-7.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg',
-        'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg'
-    ];
+    // let imgList = ['https://hugh-ferguson.com/assets/img/zine/test-page-7.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-1.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-6.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-2.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-5.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-3.jpg',
+    //     'https://hugh-ferguson.com/assets/img/zine/test-page-4.jpg'
+    // ];
 
     let current = imgList.indexOf(img1.src);
 
